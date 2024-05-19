5e4044858a5444a81bdaa1defdd977e5d3cb492c
loosiegoosey <loosiegoosey4eva@proton.me>
2024-05-19 12:09:31 -0700

    Update context_menu_improved.py

--- context_menu_improved.py
+++ context_menu_improved.py
@@ -8,11 +8,9 @@ import time
 import win32con
 import win32api
 import win32gui
-import win32com.client
 
 class ContextMenuHandler:
     def __init__(self):
-        self.hMenu = None
         self.logger = self.setup_logger()
         self.config = self.load_config()
 
@@ -45,33 +43,6 @@ class ContextMenuHandler:
             config['DEFAULT']['OutputDirectory'] = default_documents_dir
 
         return config
-
-    def add_context_menu_item(self, hMenu, menu_text, command_id):
-        win32gui.InsertMenu(hMenu, 0, win32con.MF_BYPOSITION | win32con.MF_STRING, command_id, menu_text)
-        return command_id + 1
-
-    def query_context_menu(self, hMenu, indexMenu, idCmdFirst, idCmdLast, uFlags):
-        self.hMenu = hMenu
-        command_id = idCmdFirst
-        command_id = self.add_context_menu_item(hMenu, "Append Files", command_id)
-        return command_id - idCmdFirst
-
-    def invoke_command(self, lpici):
-        if lpici.lpVerb == 0:  # "Append Files" command id
-            files = self.get_selected_files()
-            self.logger.debug(f"Selected files: {files}")
-            if files:
-                self.append_files(files)
-
-    def get_selected_files(self):
-        # Use the shell to get the selected files
-        shell = win32com.client.Dispatch("Shell.Application")
-        selected_files = []
-        for folder in shell.Windows():
-            if folder.Document.FocusedItem:
-                selected_files.append(folder.Document.FocusedItem.Path)
-        self.logger.debug(f"Selected files: {selected_files}")
-        return selected_files
 
     def append_files(self, files):
         try:
@@ -106,10 +77,14 @@ class ContextMenuHandler:
             self.logger.error(f"Error reading file {file}: {e}")
 
 def register():
-    handler = ContextMenuHandler()
-    command = f'python "{os.path.abspath(__file__)}" %*'
+    command = f'python "{os.path.abspath(__file__)}" "%1"'
     win32api.RegSetValue(win32con.HKEY_CLASSES_ROOT, r'*\\shell\\AppendFiles\\command', win32con.REG_SZ, command)
     win32api.RegSetValue(win32con.HKEY_CLASSES_ROOT, r'*\\shell\\AppendFiles', win32con.REG_SZ, 'Append Files')
 
 if __name__ == "__main__":
-    register()
+    if len(sys.argv) > 1:
+        handler = ContextMenuHandler()
+        handler.logger.debug(f"Command-line arguments: {sys.argv}")
+        handler.append_files(sys.argv[1:])
+    else:
+        register()
