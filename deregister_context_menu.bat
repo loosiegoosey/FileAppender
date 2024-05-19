e7fd2ccf9febd506dffe4ec7431122e143e06843
loosiegoosey <loosiegoosey4eva@proton.me>
2024-05-19 12:57:01 -0700

    making register and deregister scripts into batch files

---
+++ deregister_context_menu.bat
@@ -0,0 +1,3 @@
+@echo off
+python "C:\Users\Yuriy\Documents\GitHub\FileAppender\deregister_context_menu.py"
+pause
---
+++ register_context_menu.bat
@@ -0,0 +1,3 @@
+@echo off
+python "C:\Users\Yuriy\Documents\GitHub\FileAppender\context_menu_improved.py"
+pause
